779bfaaf9e288279552ad5faef4d5714ed599588
maxim <weise2004@gmail.com>
2021-10-19 01:59:32 +0300

    practis 2

--- js/sript.js
+++ js/sript.js
@@ -4,32 +4,53 @@ let number0films = +prompt("Сколько фильмов вы уже посмо
 
 console.log(number0films);
 
-
 // let qes1 = prompt("Один из последних просмотренных фильмов?:"),
 //     qes2 = +prompt("На сколько оцените его?:"),
 //     qes3 = prompt("Один из последних просмотренных фильмов?:"),
 //     qes4 = +prompt("На сколько оцените его?:");
 
 
+// const personalMuvieDB = {
+
+//     count: number0films,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
 
-const personalMuvieDB = {
+// for (let i = 0; i < 2; i++) {
+//     const qes1 = prompt("Один из последних просмотренных фильмов?:"),
+//           qes2 = prompt("На сколько оцените его?:");
 
-    count: number0films,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+//           personalMuvieDB.movies[qes1] = [qes2];
+// }
 
-for (let i = 0; i < 2; i++) {
-    const qes1 = prompt("Один из последних просмотренных фильмов?:"),
-          qes2 = prompt("На сколько оцените его?:");
+// console.log(personalMuvieDB);
 
-          personalMuvieDB.movies[qes1] = [qes2];
+
+
+
+for (let i = 0; i <= 10; i++) {
+    const ques1 = prompt("Какой ваш фильм(больше 50 символов нельзя):");
+    if (ques1 == 0) {
+        break;
+    }
+    else if (ques1.length <= 10) {
+        console.log("ЗАебись");
+    }
+    else {
+        if (i == 10) {
+            console.log("У вас есть только 10 попыток");
+        }
+    }
 }
 
-console.log(personalMuvieDB);
+
+
+
+
 
 
 
